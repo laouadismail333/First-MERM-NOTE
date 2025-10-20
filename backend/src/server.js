86107417ab9747494a5fb64990dcca9e14e6b623
ismail <ismail@gmail.com>
2025-10-20 12:05:50 +0100

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,19 @@ import connectDB from '../src/config/db.js'
 import dotenv from 'dotenv'
 import rateLimiter from './middleware/rateLimiter.js'
 import cors from 'cors'
+import path from 'path'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT
+const __dirname = path.resolve()
 
-app.use(cors({
-  origin:"http://localhost:5173",
-}))
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({
+    origin:"http://localhost:5173",
+  }))
+}
 
 //middleware
 app.use(express.json()) 
@@ -24,6 +28,15 @@ app.use(rateLimiter)
 // })
 
 app.use('/api/notes',notesRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'../frontend/dist')))
+
+  app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../frontend','dist','index.html'))
+  })
+
+}
 
 connectDB().then(()=>{
   app.listen(PORT,()=>{
